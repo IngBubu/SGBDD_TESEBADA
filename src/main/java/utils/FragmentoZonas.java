32b750ec70d3c4a8570276b8d1e3739515cd3182
David Carmona Teran <l20170623@culiacan.tecnm.mx>
2025-03-05 22:38:47 -0700

    Distribucion de estados por zona corregida

--- src/main/java/utils/FragmentoZonas.java
+++ src/main/java/utils/FragmentoZonas.java
@@ -9,37 +9,30 @@ public class FragmentoZonas {
     static {
         // 🔹 Mapeo de estados a zonas (ahora en minúsculas)
         mapaEstadosZona.put("durango", "ZonaNorte");
-        mapaEstadosZona.put("cdmx", "ZonaNorte");
+        mapaEstadosZona.put("baja california sur", "ZonaNorte");
         mapaEstadosZona.put("chihuahua", "ZonaNorte");
-        mapaEstadosZona.put("estado de mexico", "ZonaNorte");
+        mapaEstadosZona.put("sinaloa", "ZonaNorte");
         mapaEstadosZona.put("baja california", "ZonaNorte");
-        mapaEstadosZona.put("queretaro", "ZonaNorte");
         mapaEstadosZona.put("coahuila", "ZonaNorte");
         mapaEstadosZona.put("tamaulipas", "ZonaNorte");
         mapaEstadosZona.put("sonora", "ZonaNorte");
-        mapaEstadosZona.put("morelos", "ZonaNorte");
 
         mapaEstadosZona.put("zacatecas", "ZonaCentro");
         mapaEstadosZona.put("hidalgo", "ZonaCentro");
         mapaEstadosZona.put("queretaro", "ZonaCentro");
-        mapaEstadosZona.put("puebla", "ZonaCentro");
-        mapaEstadosZona.put("nuevo leon", "ZonaCentro");
+        mapaEstadosZona.put("guanajuato", "ZonaCentro");
+        mapaEstadosZona.put("Tlaxcala", "ZonaCentro");
         mapaEstadosZona.put("cdmx", "ZonaCentro");
         mapaEstadosZona.put("morelos", "ZonaCentro");
-        mapaEstadosZona.put("yucatan", "ZonaCentro");
+        mapaEstadosZona.put("aguascalientes", "ZonaCentro");
         mapaEstadosZona.put("san luis potosi", "ZonaCentro");
         mapaEstadosZona.put("michoacan", "ZonaCentro");
 
         mapaEstadosZona.put("oaxaca", "ZonaSur");
         mapaEstadosZona.put("veracruz", "ZonaSur");
         mapaEstadosZona.put("guerrero", "ZonaSur");
-        mapaEstadosZona.put("zacatecas", "ZonaSur");
         mapaEstadosZona.put("quintana roo", "ZonaSur");
-        mapaEstadosZona.put("queretaro", "ZonaSur");
-        mapaEstadosZona.put("guanajuato", "ZonaSur");
-        mapaEstadosZona.put("estado de mexico", "ZonaSur");
         mapaEstadosZona.put("campeche", "ZonaSur");
-        mapaEstadosZona.put("hidalgo", "ZonaSur");
         mapaEstadosZona.put("tabasco", "ZonaSur");
         mapaEstadosZona.put("chiapas", "ZonaSur");
     }
